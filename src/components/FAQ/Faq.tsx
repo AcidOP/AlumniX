4f2d0dfcdb931bb9a9cd8c67e373d742c8b5104c
AcidOP <zeeshanalivr46@gmail.com>
2024-08-24 18:57:25 +0530

    fix: ui issues

--- src/components/FAQ/Faq.tsx
+++ src/components/FAQ/Faq.tsx
@@ -8,7 +8,7 @@ interface IProps {
 
 const Faq = ({ question, answer, className }: IProps) => {
   return (
-    <details className={cn("open:bg-green-50 duration-300", className)}>
+    <details className={cn("open:bg-green-50 duration-300 mx-5 lg:mx-0", className)}>
       <summary className="bg-inherit px-5 py-3 text-lg cursor-pointer font-medium">{question}</summary>
       <div className="bg-white px-5 py-3 leading-relaxed">
         <p>{answer}</p>
--- src/components/FAQ/index.tsx
+++ src/components/FAQ/index.tsx
@@ -3,7 +3,8 @@ import faqArray from "./questions"
 
 const Index = () => {
   return (
-    <div className="container mx-auto">
+    <div className="container mx-auto mb-16">
+      <h1 className="py-10 text-4xl text-center font-bold">Frequently Asked Questions</h1>
       {
         faqArray.map((faq, index) => {
           return (
