dddc6d38ae1f6e7ebe791097969a90ffa990255e
AcidOP <zeeshanalivr46@gmail.com>
2024-08-25 10:19:42 +0530

    feat: updated questoins

--- src/components/FAQ/questions.tsx
+++ src/components/FAQ/questions.tsx
@@ -19,41 +19,16 @@ const faqArray: FAQ[] = [
     answer:
       "Donations can be made securely through our Donation Portal, accessible via the 'Donate' button.",
   },
-  // {
-  //   question: "How do I connect with other alumni in my industry?",
-  //   answer:
-  //     "Use our Networking Hub to filter and connect with alumni based on industry, location, and shared interests.",
-  // },
-  // {
-  //   question: "How do I post a job opportunity on the Job Portal?",
-  //   answer:
-  //     "Simply log in to your account, go to the Job Portal section, and click 'Post a Job' to fill out the necessary details.",
-  // },
-  // {
-  //   question: "How can I search for alumni in my area?",
-  //   answer:
-  //     "Use the Alumni Directory’s search function to find alumni by location, graduation year, field of study, and more.",
-  // },
-  // {
-  //   question: "How do I share my success story with the alumni community?",
-  //   answer:
-  //     "You can submit your success story through the 'Share Your Story' feature on your profile page, and we’ll review it for publication.",
-  // },
-  // {
-  //   question: "Where can I find information about upcoming alumni events?",
-  //   answer:
-  //     "Upcoming events are listed in the 'Events and Reunions' section. You can also register for events directly from this page.",
-  // },
-  // {
-  //   question: "Is there a mobile app available for the alumni platform?",
-  //   answer:
-  //     "Yes, our alumni platform is accessible via a mobile app, available for download on both iOS and Android devices.",
-  // },
-  // {
-  //   question: "How can I get involved in mentorship programs?",
-  //   answer:
-  //     "Visit the Mentorship section under the Networking Hub to find opportunities to mentor current students or other alumni, or to seek mentorship yourself.",
-  // },
+  {
+    question: "How do I post a job opportunity on the Job Portal?",
+    answer:
+      "Simply log in to your account, go to the Job Portal section, and click 'Post a Job' to fill out the necessary details.",
+  },
+  {
+    question: "Is there a mobile app available for the alumni platform?",
+    answer:
+      "Yes, our alumni platform is accessible via a mobile app, available for download on both iOS and Android devices.",
+  },
 ];
 
 export default faqArray;
